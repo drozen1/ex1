a57e8fa102a1c987f7c3afae6f3b8286b36a7f46
drozen <drozen@campus.technion.ac.il>
2020-04-28 02:36:21 +0300

    add get

---
+++ main2.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Dor on 28/04/2020.
+//
+
+
+#include <iostream>
+#include "main2.h"
+using namespace std;
+
+
+
+class person{
+public:
+    person(int age) : age(age) {}
+
+    friend ostream &operator<<(ostream &os, const person &person) {
+        os << "age: " << person.age;
+        return os;
+    }
+
+    int getAge() const {
+        return age;
+    }
+
+    void setAge(int age) {
+        person::age = age;
+    }
+
+    bool operator==(const person &rhs) const {
+        return age == rhs.age;
+    }
+
+    void print() {
+
+
+    }
+
+private:
+    int age;
+};
+
+int main(int argc, const char**argv) {
+    int x=32;
+    person dor= person(x);
+    person leon= person(32);
+    if(leon==dor){
+        std::cout<<dor<<endl;
+    }
+    dor.print();
+    return 1;
+}
